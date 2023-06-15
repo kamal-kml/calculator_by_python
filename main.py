51116c1373076c41f4bfaed127b43e79e2f1fb17
Kamal <kamaldn3@gmail.com>
2023-06-16 01:26:53 +0530

    input frame done

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ window.geometry("320x400")
 window.resizable(0,0)
 window.title("CALC BY KAMAL")
 
+input = StringVar()
+
+#input frame
+frame = Frame(window,height=60,width=320,bg="black")
+frame.pack(side=TOP)
+
+#display
+disp = Entry(frame,font=("arial",16,'bold'),textvariable=input,width=100,justify="right")
+disp.config(state="readonly",background="black")
+disp.grid(row=0,column=0)
+disp.pack(ipady=10)
+
 
 window.mainloop()
