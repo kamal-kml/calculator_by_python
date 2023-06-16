e86f95ab0744292d1d71cb0b2da93bf57f2df51a
Kamal <kamaldn3@gmail.com>
2023-06-16 11:51:25 +0530

    third button row added

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ btnframe.pack()
 
 #row 1
 clear = Button(btnframe,width=10,height=4,text="C")
-clear.grid(row=0,column=0,padx=1,pady=2)
+clear.grid(row=0,column=0,padx=1,pady=1)
 
 posorneg = Button(btnframe,width=10,height=4,text="+/-")
-posorneg.grid(row=0,column=1,padx=1,pady=2)
+posorneg.grid(row=0,column=1,padx=1,pady=1)
 
 mod = Button(btnframe,width=10,height=4,text="%")
-mod.grid(row=0,column=2,padx=1,pady=2)
+mod.grid(row=0,column=2,padx=1,pady=1)
 
 div = Button(btnframe,width=10,height=4,text="/")
 div.grid(row=0,column=3,padx=1,pady=1)
@@ -38,18 +38,32 @@ div.grid(row=0,column=3,padx=1,pady=1)
 # row=2
 
 seven = Button(btnframe,width=10,height=4,text="7")
-seven.grid(row=1,column=0,padx=1,pady=2)
+seven.grid(row=1,column=0,padx=1,pady=1)
 
 eight = Button(btnframe,width=10,height=4,text="8")
-eight.grid(row=1,column=1,padx=1,pady=2)
+eight.grid(row=1,column=1,padx=1,pady=1)
 
 nine = Button(btnframe,width=10,height=4,text="9")
-nine.grid(row=1,column=2,padx=1,pady=2)
+nine.grid(row=1,column=2,padx=1,pady=1)
 
 mul = Button(btnframe,width=10,height=4,text="*")
-mul.grid(row=1,column=3,padx=1,pady=2)
+mul.grid(row=1,column=3,padx=1,pady=1)
+
+
 
 # row 3
+
+four = Button(btnframe,width=10,height=4,text="4")
+four.grid(row=2,column=0)
+
+five = Button(btnframe,width=10,height=4,text="5")
+five.grid(row=2,column=1)
+
+six = Button(btnframe,width=10,height=4,text="6")
+six.grid(row=2,column=2)
+
+sub = Button(btnframe,width=10,height=4,text="-")
+sub.grid(row=2,column=3)
 
 window.mainloop()
 
