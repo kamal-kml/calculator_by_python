761a04b137819baa86591cd685186a249875c4fc
Kamal <kamaldn3@gmail.com>
2023-06-16 11:14:37 +0530

    1st row buttons added

--- main.py
+++ main.py
@@ -12,10 +12,27 @@ frame = Frame(window,height=60,width=320,bg="black")
 frame.pack(side=TOP)
 
 #display
-disp = Entry(frame,font=("arial",16,'bold'),textvariable=input,width=100,justify="right")
-disp.config(state="readonly",background="black")
+disp = Entry(frame,font=("arial",16,'bold'),textvariable=input,width=60,justify="right")
+disp.config(state="readonly")
 disp.grid(row=0,column=0)
 disp.pack(ipady=10)
 
+#buttons
+
+btnframe = Frame(window,width=318,height=350,bg="#415a77")
+btnframe.pack()
+
+
+clear = Button(btnframe,width=10,height=4,text="C")
+clear.grid(row=0,column=0,padx=2,pady=2)
+
+posorneg = Button(btnframe,width=10,height=4,text="+/-")
+posorneg.grid(row=0,column=1,padx=2,pady=2)
+
+mod = Button(btnframe,width=10,height=4,text="%")
+mod.grid(row=0,column=2,padx=2,pady=2)
+
+div = Button(btnframe,width=10,height=4,text="/")
+div.grid(row=0,column=3,padx=2,pady=2)
 
 window.mainloop()
